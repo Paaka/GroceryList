16c4b1c4a18b6386f12d80589526525cf8452383
Paaka <32105930+Paaka@users.noreply.github.com>
2020-10-28 23:51:06 +0100

    Added react router

--- grocery-list/src/App.js
+++ grocery-list/src/App.js
@@ -1,40 +1,20 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
-import AddGroceryForm from './components/molecules/AddGroceryForm';
-import GroceryListItem from './components/molecules/GroceryListItem';
-import GroceryListContainer from './components/organisms/GroceryListContainer';
-import Header from './components/organisms/Header';
+
+import ListView from './views/ListView';
+
 
 function App() {
-  const groceryList = useSelector(state => state.groceryList);
-
-  const returnUnpurchasedItems = () => {
-    return groceryList.map((listItem, index) => {
-        if(!listItem.isBought){
-          return <GroceryListItem key={listItem.id} listItem={listItem} />
-        }
-    });
-  };
-
-  const returnPurchasedItems = () => {
-    return groceryList.map((listItem, index) => {
-        if(listItem.isBought){
-          return <GroceryListItem key={listItem.id} listItem={listItem} />
-        }
-    });
-  };
-
   return (
     <div className="App">
-      <Header title="my grocery list">Heelo</Header>
-      <AddGroceryForm></AddGroceryForm>
-      <GroceryListContainer>
-        {returnUnpurchasedItems()}
-      </GroceryListContainer>
-      <GroceryListContainer>
-        {returnPurchasedItems()}
-      </GroceryListContainer>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <ListView></ListView>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ grocery-list/src/views/ListView.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Header from '../components/organisms/Header';
+import AddGroceryForm from '../components/molecules/AddGroceryForm';
+import GroceryListContainer from '../components/organisms/GroceryListContainer';
+import GroceryListItem from '../components/molecules/GroceryListItem';
+
+
+const ListView = () => {
+    const groceryList = useSelector(state => state.groceryList);
+
+    const returnSelectedItems = (logicValue) => {
+      return groceryList.map((listItem, index) => {
+          if(listItem.isBought === logicValue){
+            return <GroceryListItem key={listItem.id} listItem={listItem} />
+          }
+      });
+    };
+  
+    const returnPurchasedItems = () => {
+      return groceryList.map((listItem, index) => {
+          if(listItem.isBought){
+            return <GroceryListItem key={listItem.id} listItem={listItem} />
+          }
+      });
+    };
+
+    return(
+        <div>
+            <Header title="my grocery list">Heelo</Header>
+            <AddGroceryForm></AddGroceryForm>
+            <GroceryListContainer>
+                {returnSelectedItems(false)}
+            </GroceryListContainer>
+            <GroceryListContainer>
+                {returnSelectedItems(true)}
+            </GroceryListContainer>
+        </div>
+    );
+}
+
+export default ListView;
