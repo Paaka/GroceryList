69ed0e4f2beffc3f584c009cfb4454ff34602f66
Paaka <32105930+Paaka@users.noreply.github.com>
2020-11-10 15:40:12 +0100

    Finished Form in SignIn

--- grocery-list/src/components/molecules/LoginFormInput.js
+++ grocery-list/src/components/molecules/LoginFormInput.js
@@ -1,16 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+import DivImage from '../atoms/DivImage';
 
-const LoginFormInput = ({inputType, inputValue, setInputFn}) => {
-    
+import Fonts from '../../constants/fonts';
+import colors from '../../constants/colors';
+
+const {montserrat} = Fonts;
+
+const Wrapper = styled.div`
+    display:flex;
+    flex-direction:column;
+    width:80%;
+    margin:15px auto;
+`
+
+const StyledInput = styled.input`
+    border:none;
+    outline:none;
+`
+
+const ImageAndInputContainer = styled.div`
+    display:flex;
+    border-bottom:2px solid #ccc;
+    transition:all 0.2s ease-in;
+
+    &:focus-within{
+        border-bottom:2px solid ${colors.gradientSecondary};
+    }
+`
+
+const StyledLabel = styled.label`
+    align-self:flex-start;
+    font-family:${montserrat.fontName};
+    font-weight:${montserrat.fontWeights.bold};
+    font-size:12px;
+    color:#666;
+`
+
+
+const LoginFormInput = ({label, placeholder, inputType, inputValue, setInputFn, image}) => {
+
     const updateInputValueHanlder = (e) => {
         setInputFn(e.target.value);
     };
 
     return(
-        <div>
-            <label>Label</label>
-            <input type={inputType} value={inputValue} onChange={updateInputValueHanlder}/>
-        </div>
+        <Wrapper>
+            <StyledLabel>{label}</StyledLabel>
+            <ImageAndInputContainer>
+            <DivImage image={image} height={25} width={25} bgSize={20}/>
+            <StyledInput type={inputType} placeholder={placeholder} value={inputValue} onChange={updateInputValueHanlder}/>
+            </ImageAndInputContainer>
+        </Wrapper>
     )    
 }
 
--- grocery-list/src/components/molecules/SignIn.js
+++ grocery-list/src/components/molecules/SignIn.js
@@ -6,6 +6,8 @@ import colors from '../../constants/colors';
 import GradientButton from '../atoms/GradientButton';
 import Heading3 from '../atoms/Heading';
 import LoginFormInput from './LoginFormInput';
+import atSVG from '../../assets/SVG/arroba.svg';
+import padlockSVG from '../../assets/SVG/padlock.svg';
 
 const Wrapper = styled.div`
     animation: ${showOutOfTransparency} 1s ease-in;
@@ -29,8 +31,18 @@ const SignIn = () => {
     return(<Wrapper>
         <Heading3 textTransform="uppercase" fontSize={30}>Login</Heading3>
         <form onSubmit={submitHandler}>
-            <LoginFormInput inputType="email" inputValue={emailInput} setInputFn={setEmailInput}/>
-            <LoginFormInput inputType="password"inputValue={passwordInput} setInputFn={setPasswordInput}/>
+            <LoginFormInput label="Email"
+                            placeholder="Type your email"
+                            inputType="email"
+                            image={atSVG} 
+                            inputValue={emailInput} 
+                            setInputFn={setEmailInput}/>
+            <LoginFormInput label="Password"
+                            placeholder="Type your password"
+                            inputType="password"
+                            image={padlockSVG}
+                            inputValue={passwordInput} 
+                            setInputFn={setPasswordInput}/>
             <GradientButton firstGradientColor={colors.gradientPrimary}
                             secondGradientColor={colors.gradientSecondary} 
                             type="submit" color="white">Login</GradientButton>
